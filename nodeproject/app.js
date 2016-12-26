280ef261f732dbb4633f8eeabe2bf33ff8f4d4d4
Andee <andee.loh@gmail.com>
2016-12-26 09:57:36 +0000

    Next stage commit

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,23 @@
-var Statement = 'Hello World';
-console.log(Statement);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+// start listening for GET commands at /
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/routing', function(req, res) {
+    res.send('Routing!');
+});
+
+
+app.listen(port,function(err)
+{
+   console.log('The server is running on port: '+ port); 
+});
